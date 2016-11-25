2a6e2b39a1a63c235564697581687fc7074e712a
Vinny Sanchez <VinnySanchez87@gmail.com>
2016-11-25 14:30:42 -0500

    Modify CSS for mobile buttons.

--- dist/server.js
+++ dist/server.js
@@ -1,1 +1,88 @@
-!function(e){function t(o){if(s[o])return s[o].exports;var r=s[o]={exports:{},id:o,loaded:!1};return e[o].call(r.exports,r,r.exports,t),r.loaded=!0,r.exports}var s={};return t.m=e,t.c=s,t.p="",t(0)}([function(e,t,s){"use strict";function o(e){return e&&e.__esModule?e:{"default":e}}var r=s(1),n=o(r),u=s(2),c=(0,n["default"])(),i=process.env.PORT||8086;c.use(n["default"]["static"](process.cwd()+"/dist")),c.use(n["default"]["static"](process.cwd()+"/build")),c.use(n["default"]["static"](process.cwd()+"/public")),c.get("/",function(e,t){t.sendFile((0,u.join)(process.cwd(),"/build","index.html"))}),c.listen(i,function(){return global.console.log("Two-Blocks app listening on port "+i)})},function(e,t){e.exports=require("express")},function(e,t){e.exports=require("path")}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _express = __webpack_require__(1);
+
+	var _express2 = _interopRequireDefault(_express);
+
+	var _path = __webpack_require__(2);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	var app = (0, _express2.default)();
+
+	var SERVER_PORT_NUMBER = process.env.PORT || 8086;
+
+	app.use(_express2.default.static(process.cwd() + '/dist'));
+	app.use(_express2.default.static(process.cwd() + '/build'));
+	app.use(_express2.default.static(process.cwd() + '/public'));
+
+	app.get('/', function (req, res) {
+		// eslint-disable-line no-unused-vars
+
+		res.sendFile((0, _path.join)(process.cwd(), '/build', 'index.html'));
+	});
+
+	app.listen(SERVER_PORT_NUMBER, function () {
+		return global.console.log('Two-Blocks app listening on port ' + SERVER_PORT_NUMBER);
+	});
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	module.exports = require("express");
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	module.exports = require("path");
+
+/***/ }
+/******/ ]);
