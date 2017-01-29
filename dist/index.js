f7bf01a93f17c9090db76f4d2e3b4ac7a299ec63
Vinny Sanchez <VinnySanchez87@gmail.com>
2017-01-28 21:46:01 -0500

    Rebundle.

--- dist/index.js
+++ dist/index.js
@@ -13965,9 +13965,6 @@
 	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-	// import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-	// import ReactTransitionGroup from 'react-addons-transition-group';
-
 
 	var PROMPT_TEXT_CLASS_NAME = "prompt-text";
 
